89fa37426bb42d013e5f84ec2030b1aac69d9db6
Pujol Marcel <marcel.pujol@cotecna.es>
2022-04-06 12:40:35 +0200

    fix routes

--- projects/projects/shell/src/app/app-routing.module.ts
+++ projects/projects/shell/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
       remoteEntry: 'http://locahost:4200/remoteEntry.js',
       exposedModule: './Module'
     })
-    .then(m => m.AppModule)
+    .then(m => m.Application1Module)
   },
   {
     path: 'application2',
@@ -19,7 +19,7 @@ const routes: Routes = [
       remoteEntry: 'http://localhost:4800/remoteEntry.js',
       exposedModule: './Module'
     })
-    .then(m => m.AppModule)
+    .then(m => m.Application2Module)
   }
 ];
 
