2c09fc1e816b673284bd4b163e19ccf15496ded4
Pujol Marcel <marcel.pujol@cotecna.es>
2022-04-06 12:30:34 +0200

    adding routes

--- projects/projects/shell/src/app/app-routing.module.ts
+++ projects/projects/shell/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'application1',
+    loadChildren: () => loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://locahost:4200/remoteEntry.js',
+      exposedModule: './Module'
+    })
+    .then(m => m.AppModule)
+  },
+  {
+    path: 'application2',
+    loadChildren: () => loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4800/remoteEntry.js',
+      exposedModule: './Module'
+    })
+    .then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
