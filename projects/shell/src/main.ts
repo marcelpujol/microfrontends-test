9fb06d2cb08a3c2d367443c0c2f9625ad4af46be
Pujol Marcel <marcel.pujol@cotecna.es>
2022-04-07 18:09:04 +0200

    loading asserts from remotes

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -2,7 +2,7 @@ import { loadRemoteEntry } from '@angular-architects/module-federation';
 
 Promise.all([
    loadRemoteEntry({type: 'module', remoteEntry: 'http://localhost:3000/remoteEntry.js'}),
-	 loadRemoteEntry({type: 'module', remoteEntry: 'http://localhost:3200/remoteEntry.js'})
+	loadRemoteEntry({type: 'module', remoteEntry: 'http://localhost:3200/remoteEntry.js'})
 ])
 .catch(err => console.error('Error loading remote entries', err))
 .then(() => import('./bootstrap'))
