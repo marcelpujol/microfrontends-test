64bccc6cfea62fd6faf4b7d080b945efb76d0103
Pujol Marcel <marcel.pujol@cotecna.es>
2022-04-07 19:26:31 +0200

    show weather info on the view

--- projects/application2/src/app/app.component.ts
+++ projects/application2/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { MeteoService } from './services/meteo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'application2';
+
+  weather: any;
+
+  constructor(private meteoService: MeteoService) {
+  }
+
+  ngOnInit(): void {
+    this.getWeatherInfo();
+  }
+
+  private async getWeatherInfo() {
+    const weather$ = this.meteoService.getWeather();
+    this.weather = await lastValueFrom(weather$);
+  }
 }
