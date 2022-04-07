c32e7d3c967d92c299b72bfd41a3d1a52a5b459d
Pujol Marcel <marcel.pujol@cotecna.es>
2022-04-07 19:26:14 +0200

    adding httpclientmodule

--- projects/application2/src/app/application2.module.ts
+++ projects/application2/src/app/application2.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { APPLICATION_2_ROUTES } from './application2.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forChild(APPLICATION_2_ROUTES)
   ],
   providers: [],
